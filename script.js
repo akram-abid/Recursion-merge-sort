9d7297100682b48ac07d51b971cb3bc88bd8ed0a
akram-abid <a.abid44778@gmail.com>
2025-02-01 12:25:28 +0100

    feat: create the fibs function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hi");
+function fibs(number) {
+    const arr = [0, 1];
+    for (let i = 0; i < number - 2; i++) {
+        const newEL = arr[arr.length - 1] + arr[arr.length - 2];
+        arr.push(newEL);
+    }
+    return arr;
+}
+
